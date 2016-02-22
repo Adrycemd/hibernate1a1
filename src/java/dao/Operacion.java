bc3a5450e311a37176db3ba19c3362927a843aff
Aula51 <Aula51@A51E10.infor.com>
2016-02-22 17:41:28 +0100

    arreglado un poco el dao

--- src/java/dao/Operacion.java
+++ src/java/dao/Operacion.java
@@ -1,58 +1,58 @@
 package dao;
 
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import pojo.Persona;
+
 public class Operacion {
 
-    public String altaPersona (SessionFactory conexion, Persona persona){
-        String hash= BCrypt.hashpw(persona.getPassword(),BCrypt.gensalt());
-        Session sesion= conexion.openSession();
-        Transaction transaccion=null;
-        
+    public String altaPersona(SessionFactory conexion, Persona persona) {
+        Session sesion = conexion.openSession();
+        Transaction transaccion = null;
+
         try {
-            transaccion =sesion.beginTransaction();
-            persona.setPassword(hash);
-            persona.setDadoAlta(Boolean.FALSE);
+            transaccion = sesion.beginTransaction();
             sesion.save(persona);
             transaccion.commit();
             return "Persona dada de alta con éxito";
-        }
-        catch (HibernateException e){
-            if(transaccion!=null){
+        } catch (HibernateException e) {
+            if (transaccion != null) {
                 transaccion.rollback();
             }
             return "Error al dar de alta a la persona";
         }
     }
-    
-    public String bajaPersona (SessionFactory conexion, Persona persona){
-        Session sesion= conexion.openSession();
-        Transaction transaccion=null;
-        if (loginCorrecto(conexion, persona)){
-            try {
-                Query consulta;
-                List lista;
-                
-                transaccion=sesion.beginTransaction();
-                consulta = sesion.createQuery ("FROM persona WHERE NIF=:persona_nif");
-                consulta.setParameter("persona_nif",persona.getNif());
-                lista=consulta.list();
-                
-                Persona personaBorrable=(Persona) lista.iterator().next();
-                
-                sesion.delete(personaBorrable);
-                transaccion.commit();
-                return "Persona borrada del sistema con éxito.";
-            
-            }   catch (HibernateException e){
-                if (transaccion!=null){
-                  transaccion.rollback();  
-                }
-                return "Error al dar de baja a la persona.";
 
+    public String bajaPersona(SessionFactory conexion, Persona persona) {
+        Session sesion = conexion.openSession();
+        Transaction transaccion = null;
+        try {
+            Query consulta;
+            List lista;
+
+            transaccion = sesion.beginTransaction();
+            consulta = sesion.createQuery("FROM persona WHERE DNI=:persona_dni");
+            consulta.setParameter("persona_dni", persona.getDni());
+            lista = consulta.list();
+
+            Persona personaBorrable = (Persona) lista.iterator().next();
+
+            sesion.delete(personaBorrable);
+            transaccion.commit();
+            return "Persona borrada del sistema con éxito.";
+
+        } catch (HibernateException e) {
+            if (transaccion != null) {
+                transaccion.rollback();
             }
+            return "Error al dar de baja a la persona.";
+
         }
-        else {
-            return "Nombre de usuario y/o contraseña incorrectos."
+
     }
-        
-    
+
 }
